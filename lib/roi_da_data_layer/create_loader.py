cbd5a789f54066606c29e65eb12b3fa0b00bfa1e
Taekyung Kim <tkkim93@kaist.ac.kr>
2019-10-04 18:02:05 +0900

    Add files via upload

---
+++ lib/roi_da_data_layer/create_loader.py
@@ -0,0 +1,48 @@
+import torch
+import os
+from roi_da_data_layer.roidb import combined_roidb
+from roi_da_data_layer.roibatchLoader import roibatchLoader
+from torch.utils.data.sampler import Sampler
+
+class sampler(Sampler):
+    def __init__(self, train_size, batch_size):
+        self.num_data = train_size
+        self.num_per_batch = int(train_size / batch_size)
+        self.batch_size = batch_size
+        self.range = torch.arange(0,batch_size).view(1, batch_size).long()
+        self.leftover_flag = False
+        if train_size % batch_size:
+            self.leftover = torch.arange(self.num_per_batch*batch_size, train_size).long()
+            self.leftover_flag = True
+
+    def __iter__(self):
+        rand_num = torch.randperm(self.num_per_batch).view(-1,1) * self.batch_size
+        self.rand_num = rand_num.expand(self.num_per_batch, self.batch_size) + self.range
+        self.rand_num_view = self.rand_num.view(-1)
+
+        if self.leftover_flag:
+            self.rand_num_view = torch.cat((self.rand_num_view, self.leftover),0)
+
+        return iter(self.rand_num_view)
+
+    def __len__(self):
+        return self.num_data
+
+def create_dataloader(imdb_name, args):
+    imdb, roidb, ratio_list, ratio_index = combined_roidb(args.imdb_name)
+    train_size = len(roidb)
+
+    print('{:d} roidb entries'.format(len(roidb)))
+
+    output_dir = args.save_dir + "/" + args.net + "/" + args.dataset
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    sampler_batch = sampler(train_size, args.batch_size)
+
+    dataset = roibatchLoader(roidb, ratio_list, ratio_index, args.batch_size, \
+                             imdb.num_classes, training=True)
+
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size,
+                                             sampler=sampler_batch, num_workers=args.num_workers)
+    return dataloader, train_size, imdb
